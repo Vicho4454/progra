ee248201938774a7c693b99054504d2dee9821c1
Vicho4454 <91471233+Vicho4454@users.noreply.github.com>
2024-10-29 16:03:12 -0300

    Update untitled22.py

--- untitled22.py
+++ untitled22.py
@@ -19,7 +19,9 @@ st.header('!Hola, Streamlit!')
 st.write('Esto es una aplicación simple')
 st.image('logo.png')
 if st.button('Presiona Aqui',key = 1):
-    st.write('Has presionado el botón')
+    main = cargar_datos(archivo)
+    msg_counts = main.groupby('TIPO')['MSG'].count()
+    st.bar_chart(msg_counts,x_label = 'Tipos de mensaje', y_label = 'Cantidad')
 
 input = st.text_input('Escribe algo',key = 2)
 st.write('Escribiste:', input)
@@ -29,12 +31,8 @@ st.sidebar.header('!Hola, Barra Lateral!')
 st.sidebar.write('Esto es una barra lateral')
 st.sidebar.image('logo.png')
 
-#if st.sidebar.button('Presiona Aqui',key = 4):
-    #st.sidebar.write('Has presionado el botón')
 if st.sidebar.button('Presiona Aqui',key = 4):
-    main = cargar_datos(archivo)
-    msg_counts = main.groupby('TIPO')['MSG'].count()
-    st.bar_chart(msg_counts,x_label = 'Tipos de mensaje', y_label = 'Cantidad')
+    st.sidebar.write('Has presionado el botón')
     
 
 input = st.sidebar.text_input('Escribe algo',key = 3)
