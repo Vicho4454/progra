a4e4b75e85272e65e71f53d07d7f8974db415508
Vicho4454 <91471233+Vicho4454@users.noreply.github.com>
2024-10-29 16:04:37 -0300

    Update untitled22.py

--- untitled22.py
+++ untitled22.py
@@ -15,8 +15,9 @@ def cargar_datos(archivo):
 
 
 st.title('Mi primerra aplicación de Streamlit')
-st.header('!Hola, Streamlit!')
-st.write('Esto es una aplicación simple')
+st.title('El gonza es entero Cola')
+#st.header('!Hola, Streamlit!')
+#st.write('Esto es una aplicación simple')
 st.image('logo.png')
 if st.button('Presiona Aqui',key = 1):
     main = cargar_datos(archivo)
