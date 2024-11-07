40e78607670fe9ec07a614f981ad938a62499d2b
Vicho4454 <91471233+Vicho4454@users.noreply.github.com>
2024-11-06 23:41:20 -0300

    Update untitled22.py

--- untitled22.py
+++ untitled22.py
@@ -88,7 +88,7 @@ def graficar_trayectoria(t):
 st.title("Visualización de la trayectoria de T y P y distancia d(t) entre P y S")
 
 # Slider para el parámetro t en la trayectoria de T y P
-st.sidebar.head('Valor modificable de t en el intervalo [0,365]')
+st.sidebar.header('Valor modificable de t en el intervalo [0,365]')
 t = st.sidebar.slider("Valor de t (días)", min_value=0, max_value=365, step=1, value=0)
 
 # Graficar la trayectoria
